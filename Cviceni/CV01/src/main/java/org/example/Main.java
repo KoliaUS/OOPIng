7eaf7c9dbf060610d6ba64ce451063d4e6a6d64d
nicolaeceabin <kolia.ceabin@gmail.com>
2024-01-13 22:38:50 +0100

    Commands, EndMassage

---
+++ Cviceni/CV01/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Deklarujte si Interger, priradime mu hodnotu 5 a vypiseme
+        int a=5;
+        System.out.printf("Hodnota v a je:"+a);
+    }
+}
